7a196087811112f5e3b8d7b88d0d616ef2f0986a
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-10 13:42:08 -0300

    Add element ui

---
+++ client/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <img id="title" src="https://fontmeme.com/permalink/210209/e8a2241a22b09252fc2de3b8c2864bc9.png" alt="big-time-rush-font" border="0">
+  <br>
+  <br>
+  <br>
+  <div class="container">
+    <div class="row">
+      <img alt="Javascript" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"/>
+      <img alt="VueJs" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png"/>
+      <img alt="Nodejs" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png" />
+      <img alt="MongoDB" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"/>
+      <img alt="Git" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"/> 
+    </div>
+  </div>
+  <br>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #title {
+    width: 400px !important;
+  }
+  img {
+    margin: auto;
+    width: 30px;
+  }
+</style>
