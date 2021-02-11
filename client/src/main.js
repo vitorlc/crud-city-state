2116ba9ef5cdd24259bb32372a6d2031c96b1cfd
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-11 08:43:15 -0300

    Change element-ui to pt-br

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import './styles.scss'
 import Swal from 'sweetalert2'
+import lang from 'element-ui/lib/locale/lang/pt-br'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale: lang })
 
 Vue.prototype.$swal = Swal
 Vue.config.productionTip = false
