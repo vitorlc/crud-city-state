008c7fc2706bba08d5c682516528f427e200396c
vitorlc <vitor_cirqueira1@hotmail.com>
2021-02-11 10:20:55 -0300

    Add simple error treatment

--- client/src/main.js
+++ client/src/main.js
@@ -10,6 +10,18 @@ Vue.use(ElementUI, { locale: lang })
 Vue.prototype.$swal = Swal
 Vue.config.productionTip = false
 
+Vue.config.errorHandler = (err, vm, info) => {
+  // err: error trace
+  // vm: component in which error occured
+  // info: Vue specific error information such as lifecycle hooks, events etc.
+  if(err?.response?.data?.error)
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${err.response.data.msg}`,
+    })
+};
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
